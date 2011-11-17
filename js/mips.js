1b889070b33afc9637d4f29ad1a9d5fabc23a57c
Brian Reber <reber.brian@gmail.com>
2011-11-17 17:10:50 -0600

    Adding more functionality to simulator

--- js/mips.js
+++ js/mips.js
@@ -1,5 +1,6 @@
 var registerKey     = "reg";
 var programCounter  = "pc";
+var memKey          = "mem";
 
 /**
  * Performs an add operation
@@ -122,6 +123,18 @@ function blez(a, target) {
 };
 
 /**
+ * Performs a branch if a < b
+ */
+function blt(a, b, target) {
+    var iA = getRegisterValues(registerKey + a);
+    var iB = getRegisterValues(registerKey + b);
+    
+    if (iA < iB) {
+        branch(target);
+    }
+};
+
+/**
  * Performs a branch if a < 0
  */
 function bltz(a, target) {
@@ -200,17 +213,233 @@ function jr(a) {
 };
 
 /**
+ * Performs a load byte operation
+ */
+function lb(a, offset, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	if (offset === null) {
+        offset = 0;
+    } else if (typeof(offset) !== Number) {
+        offset = new Number(offset);
+    }
+	
+	iA += offset;
+	
+	localStorage.setItem(registerKey + dest, getMemoryValue(iA));
+};
+
+/**
+ * Performs a load upper immediate operation
+ */
+function lui(a, dest) {
+    var iA = getImmediateValue(a);
+	localStorage.setItem(registerKey + dest, (iA << 16));
+};
+
+/**
+ * Performs a load word operation
+ */
+function lw(a, offset, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	if (offset === null) {
+        offset = 0;
+    } else if (typeof(offset) !== Number) {
+        offset = new Number(offset);
+    }
+	
+	iA += offset;
+	
+	// Divide by 4 to get word address, then multiply by 4 to get aligned
+	// word address
+	iA >> 2;
+	iA << 2;
+	
+	// Get each byte individually
+	var toStore = getMemoryValue(iA);
+	toStore |= (getMemoryValue(iA + 1) << 8);
+	toStore |= (getMemoryValue(iA + 2) << 16);
+	toStore |= (getMemoryValue(iA + 3) << 24);
+	
+	localStorage.setItem(registerKey + dest, toStore);
+};
+
+/**
+ * Performs an mult operation
+ */
+function mult(a, b, dest) {
+    var iA = getRegisterValues(registerKey + a);
+    var iB = getRegisterValues(registerKey + b);
+
+	localStorage.setItem(registerKey + dest, iA * iB);
+};
+
+/**
+ * Performs an or operation
+ */
+function or(a, b, dest) {
+    var iA = getRegisterValues(registerKey + a);
+    var iB = getRegisterValues(registerKey + b);
+
+	localStorage.setItem(registerKey + dest, iA | iB);
+};
+
+/**
+ * Performs an or immediate operation
+ */
+function ori(a, b, dest) {
+    var iA = getRegisterValues(registerKey + a);
+    var iB = getImmediateValue(b);
+
+	localStorage.setItem(registerKey + dest, iA | iB);
+};
+
+/**
+ * Performs a store byte operation
+ */
+function sb(a, offset, data) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getRegisterValues(registerKey + data);
+	
+	if (offset === null) {
+        offset = 0;
+    } else if (typeof(offset) !== Number) {
+        offset = new Number(offset);
+    }
+	
+	iA += offset;
+	
+	// Get the lowest Byte
+	iB &= 0x000000FF;
+	
+	localStorage.setItem(memKey + iA, iB);
+};
+
+/**
+ * Performs a shift left logical operation
+ */
+function sll(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getImmediateValue(b);
+	
+	localStorage.setItem(registerKey + dest, iA << iB);
+};
+
+/**
+ * Performs a shift left logical variable operation
+ */
+function sllv(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getRegisterValues(registerKey + b);
+	
+	localStorage.setItem(registerKey + dest, iA << iB);
+};
+
+/**
+ * Performs a shift right arithmetic operation
+ */
+function sra(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getImmediateValue(b);
+	
+	localStorage.setItem(registerKey + dest, iA >> iB);
+};
+
+/**
+ * Performs a shift right logical operation
+ */
+function srl(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getImmediateValue(b);
+	
+	localStorage.setItem(registerKey + dest, iA >>> iB);
+};
+
+/**
+ * Performs a shift right logical variable operation
+ */
+function srlv(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getRegisterValues(registerKey + b);
+	
+	localStorage.setItem(registerKey + dest, iA >>> iB);
+};
+
+/**
+ * Performs a sub operation
+ */
+function sub(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getRegisterValues(registerKey + b);
+	
+	localStorage.setItem(registerKey + dest, iA - iB);
+};
+
+/**
+ * Performs a store word operation
+ */
+function sw(a, offset, data) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getRegisterValues(registerKey + data);
+	
+	if (offset === null) {
+        offset = 0;
+    } else if (typeof(offset) !== Number) {
+        offset = new Number(offset);
+    }
+	
+	iA += offset;
+	
+	// Divide by 4 to get word address, then multiply by 4 to get aligned
+	// word address
+	iA >> 2;
+	iA << 2;
+	
+	localStorage.setItem(memKey + iA, iB & 0x000000FF);
+	localStorage.setItem(memKey + (iA + 1), iB & 0x0000FF00);
+	localStorage.setItem(memKey + (iA + 2), iB & 0x00FF0000);
+	localStorage.setItem(memKey + (iA + 3), iB & 0xFF000000);
+};
+
+/**
+ * Performs a xor operation
+ */
+function xor(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getRegisterValues(registerKey + b);
+	
+	localStorage.setItem(registerKey + dest, iA ^ iB);
+};
+
+/**
+ * Performs a xori operation
+ */
+function xori(a, b, dest) {
+	var iA = getRegisterValues(registerKey + a);
+	var iB = getImmediateValue(b);
+	
+	localStorage.setItem(registerKey + dest, iA ^ iB);
+};
+
+/**
  * Performs a branch.
  *
  * New PC = CurrentPC + target
  */
 function branch(target) {
     var currentPC = getRegisterValues(programCounter);
-    currentPC += target;
-    
-    localStorage.setItem(programCounter, currentPC);
+	if (isNaN(currentPC)) {
+		currentPC = 0;
+	}
+	// Make sure target is Number not string
+	if (target === null) {
+        target = 0;
+    } else if (typeof(target) !== Number) {
+        target = new Number(target);
+    }
 	
-	updateUI();
+    currentPC += target;
+	
+    localStorage.setItem(programCounter, currentPC);
 };
 
 function incrementPC() {
@@ -230,11 +459,23 @@ function getRegisterValues(a) {
 };
 
 function getImmediateValue(a) {
-    var iA = a;
+    var iA = new Number(a);
         
     if (a === "" || a === undefined || a === '0' || isNaN(a) || a === "") {
         iA = 0;
 	}
+    
+    return iA;
+};
+
+function getMemoryValue(byteAddr) {
+    var iA = localStorage.getItem(memKey + byteAddr);
+    
+    if (iA === null) {
+        iA = 0;
+    } else if (typeof(iA) !== Number) {
+        iA = new Number(iA);
+    }
     
     return iA;
 };
--- js/simulate.js
+++ js/simulate.js
@@ -1,4 +1,5 @@
 window.assembledInstructions = [];
+window.labels = [];
 
 $().ready(function() {
 	$("#runStep").attr("disabled", "disabled");
@@ -12,11 +13,20 @@ $().ready(function() {
 		$("#runStep").removeAttr("disabled");
 		$("#runAll").removeAttr("disabled");
 		localStorage.clear();
+		localStorage.setItem(programCounter, 0);
+		
+		//localStorage.setItem(memKey + 0, 4);
+		//localStorage.setItem(memKey + 4, 5);
+		//localStorage.setItem(memKey + 8, -1);
+		//localStorage.setItem(memKey + 12, 15);
+		//localStorage.setItem(memKey + 16, 7);
 	});
 	
 	$("#runStep").click(function() {
 		var currentPC = getRegisterValues(programCounter);
 		if (currentPC < window.assembledInstructions.length) {
+			window.editor.setLineClass(new Number(currentPC), null);
+			window.editor.setLineClass(currentPC + 1, 'activeline');
 			window.assembledInstructions[currentPC]();
 		} else {
 			$("#messages").append("Out of instructions\n");
@@ -24,6 +34,7 @@ $().ready(function() {
 			$("#runStep").attr("disabled", "disabled");
 			$("#runAll").attr("disabled", "disabled");
 		}
+		updateUI();
 	});
 	
 	$("#runAll").click(function() {
@@ -32,7 +43,7 @@ $().ready(function() {
 			window.assembledInstructions[currentPC]();
 			currentPC = getRegisterValues(programCounter);
 		}
-		
+		updateUI();
 		$("#messages").append("Out of instructions\n");
 		$("#assemble").removeAttr("disabled");
 		$("#runStep").attr("disabled", "disabled");
@@ -40,6 +51,12 @@ $().ready(function() {
 	});
 });
 
+/**
+ * Go through the text box, line by line, parsing the instructions
+ * and adding them to the instruction memory.
+ *
+ * Essentially 'assembling' the assembly code
+ */
 function parseInstructions(text) {
 	var end = text.search("\n");
 	if (end === -1) {
@@ -56,23 +73,105 @@ function parseInstructions(text) {
 	}
 };
 
+/**
+ * Parse a line of assembly code, and add it to our 'instruction memory'
+ */
 function individualInstruction(line) {
+	// Remove comments...
+	line = line.replace(/#.+$/, '');
+	
+	// Parse Labels
+	if (line.indexOf(":") !== -1) {
+		var label = {};
+		label.label = line.substr(0, line.indexOf(":"));
+		label.instr = window.assembledInstructions.length;
+		window.labels.push(label);
+		
+		// Update the line with the label removed, and continue processing
+		line = line.substr(line.indexOf(":") + 1);
+		console.log("Found label " + label.label + " at instruction " + label.instr + " ");
+	}
+	
 	var tokens = line.split(" ");
 	
-	for (var i = 1; i < tokens.length; i++) {
-		tokens[i] = tokens[i].replace(",", "").replace("$", "");
+	for (var i = 0; i < tokens.length; i++) {
+		tokens[i] = tokens[i].replace(",", "").replace("$", "").replace(/\s|\t/g, "");
 	}
 	
 	switch (tokens[0]) {
-		case "addi" : window.assembledInstructions.push(function() { addi(tokens[2], tokens[3], tokens[1]); incrementPC(); }); break;
-		case "add" : window.assembledInstructions.push(function() { add(tokens[2], tokens[3], tokens[1]); incrementPC(); }); break;
-		case "and" : window.assembledInstructions.push(function() { and(tokens[2], tokens[3], tokens[1]); incrementPC(); }); break;
+		case "add" : window.assembledInstructions.push(function() { incrementPC(); add(tokens[2], tokens[3], tokens[1]); }); break;
+		case "addi" : window.assembledInstructions.push(function() { incrementPC(); addi(tokens[2], tokens[3], tokens[1]); }); break;
+		case "addiu" : window.assembledInstructions.push(function() { incrementPC(); addiu(tokens[2], tokens[3], tokens[1]); }); break;
+		case "addu" : window.assembledInstructions.push(function() { incrementPC(); addu(tokens[2], tokens[3], tokens[1]); }); break;
+		case "and" : window.assembledInstructions.push(function() { incrementPC(); and(tokens[2], tokens[3], tokens[1]); }); break;
+		case "andi" : window.assembledInstructions.push(function() { incrementPC(); andi(tokens[2], tokens[3], tokens[1]); }); break;
+		// Branches find Labels...
+		case "beq" : window.assembledInstructions.push(function() { incrementPC(); beq(tokens[1], tokens[2], getLabelLocation(tokens[3], true)); }); break;
+		case "bgez" : window.assembledInstructions.push(function() { incrementPC(); bgez(tokens[1], getLabelLocation(tokens[2], true)); }); break;
+		case "bgezal" : window.assembledInstructions.push(function() { incrementPC(); bgezal(tokens[1], getLabelLocation(tokens[2], true)); }); break;
+		case "bgtz" : window.assembledInstructions.push(function() { incrementPC(); bgtz(tokens[1], getLabelLocation(tokens[2], true)); }); break;
+		case "blez" : window.assembledInstructions.push(function() { incrementPC(); blez(tokens[1], getLabelLocation(tokens[2], true)); }); break;
+		// Pseudo Instruction...
+		case "blt" : window.assembledInstructions.push(function() { incrementPC(); blt(tokens[1], tokens[2], getLabelLocation(tokens[3], true)); }); break;
+		case "bltz" : window.assembledInstructions.push(function() { incrementPC(); bltz(tokens[1], getLabelLocation(tokens[2], true)); }); break;
+		case "bltzal" : window.assembledInstructions.push(function() { incrementPC(); bltzal(tokens[1], getLabelLocation(tokens[2], true)); }); break;
+		case "bne" : window.assembledInstructions.push(function() { incrementPC(); bne(tokens[1], tokens[2], getLabelLocation(tokens[3], true)); }); break;
+		case "div" : window.assembledInstructions.push(function() { incrementPC(); div(tokens[2], tokens[3], tokens[1]); }); break;
+		// DIVU
+		case "j" : window.assembledInstructions.push(function() { incrementPC(); j(getLabelLocation(tokens[1], false)); }); break;
+		case "jal" : window.assembledInstructions.push(function() { incrementPC(); jal(getLabelLocation(tokens[1], false)); }); break;
+		case "jr" : window.assembledInstructions.push(function() { incrementPC(); jr(tokens[1]); }); break;
+		// LB
+		case "lb" : window.assembledInstructions.push(function() { incrementPC(); lb(tokens[2].substr(tokens[2].indexOf("(") + 1, tokens[2].indexOf(")") - 2), tokens[2].substr(0, tokens[2].indexOf("(")), tokens[1]); }); break;
+		case "lui" : window.assembledInstructions.push(function() { incrementPC(); lui(tokens[2], tokens[1]); }); break;
+		// LW
+		case "lw" : window.assembledInstructions.push(function() { incrementPC(); lw(tokens[2].substr(tokens[2].indexOf("(") + 1, tokens[2].indexOf(")") - 2), tokens[2].substr(0, tokens[2].indexOf("(")), tokens[1]); }); break;
+		case "mul" : window.assembledInstructions.push(function() { incrementPC(); mult(tokens[2], tokens[3], tokens[1]); }); break;
+		case "mult" : window.assembledInstructions.push(function() { incrementPC(); mult(tokens[2], tokens[3], tokens[1]); }); break;
+		// MULTU
+		case "or" : window.assembledInstructions.push(function() { incrementPC(); or(tokens[2], tokens[3], tokens[1]); }); break;
+		case "ori" : window.assembledInstructions.push(function() { incrementPC(); ori(tokens[2], tokens[3], tokens[1]); }); break;
+		// SB
+		case "sb" : window.assembledInstructions.push(function() { incrementPC(); sb(tokens[2].substr(tokens[2].indexOf("(") + 1, tokens[2].indexOf(")") - 2), tokens[2].substr(0, tokens[2].indexOf("(")), tokens[1]); }); break;
+		case "sll" : window.assembledInstructions.push(function() { incrementPC(); sll(tokens[2], tokens[3], tokens[1]); }); break;
+		case "sllv" : window.assembledInstructions.push(function() { incrementPC(); sllv(tokens[2], tokens[3], tokens[1]); }); break;
+		// SLT
+		// SLTI
+		// SLTIU
+		// SLTU
+		case "sra" : window.assembledInstructions.push(function() { incrementPC(); sra(tokens[2], tokens[3], tokens[1]); }); break;
+		case "srl" : window.assembledInstructions.push(function() { incrementPC(); srl(tokens[2], tokens[3], tokens[1]); }); break;
+		case "srlv" : window.assembledInstructions.push(function() { incrementPC(); srlv(tokens[2], tokens[3], tokens[1]); }); break;
+		case "sub" : window.assembledInstructions.push(function() { incrementPC(); sub(tokens[2], tokens[3], tokens[1]); }); break;
+		// SUBU
+		case "sw" : window.assembledInstructions.push(function() { incrementPC(); sw(tokens[2].substr(tokens[2].indexOf("(") + 1, tokens[2].indexOf(")") - 2), tokens[2].substr(0, tokens[2].indexOf("(")), tokens[1]); }); break;
+		case "xor" : window.assembledInstructions.push(function() { incrementPC(); xor(tokens[2], tokens[3], tokens[1]); }); break;
+		case "xori" : window.assembledInstructions.push(function() { incrementPC(); xori(tokens[2], tokens[3], tokens[1]); }); break;
 		default : {
-			alert("Error");
+			if (!(tokens[0] === null || "" === tokens[0])) {
+				window.assembledInstructions.push(function() { incrementPC(); });
+				console.log("Unknown Instruction " + tokens[0]);
+			}
 		}
 	}
 };
 
+/**
+ * Get the offset from the CurrentPC for the given label
+ */
+function getLabelLocation(label, diff) {
+	for (var i = 0; i < window.labels.length; i++) {
+		if (window.labels[i].label === label) {
+			return diff ? (window.labels[i].instr - getRegisterValues(programCounter)) : window.labels[i].instr;
+		}
+	}
+	
+	return 0;
+};
+
+/**
+ * Update all registers and PC values on the UI
+ */
 function updateUI() {
 	for (var i = 0; i < 31; i++) {
 		$("#r" + i + "val").html("<span>" + getRegisterValues(registerKey + i) + "</span>");
